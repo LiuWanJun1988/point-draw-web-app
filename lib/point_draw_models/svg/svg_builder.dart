97aa1b87a84494ed18f2dc219e83fdbd2f961961
Ko812 <97655559+Ko812@users.noreply.github.com>
2022-07-26 11:02:09 +0800

    Update svg_builder.dart
    Include building up the svg file and output flow.

--- lib/point_draw_models/svg/svg_builder.dart
+++ lib/point_draw_models/svg/svg_builder.dart
@@ -1,32 +1,92 @@
+import 'package:flutter/material.dart';
+
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:pointdraw/point_draw_models/utilities/svg_utils.dart';
 import 'package:pointdraw/point_draw_models/point_draw_objects.dart';
 
 import 'dart:html';
 
 class SVGBuilder {
-  final double version = 1.0;
+  final double xmlVersion = 1.0;
+
+  final double svgVersion = 1.1;
 
   final String docType = "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd";
 
-  int? width;
-
-  int? height;
-
-  SVGBuilder({this.width, this.height, this.title});
+  SVGBuilder();
 
   String? title;
 
   String? desc;
 
-  List<SVGPointDrawElement> canvasElement = [];
+  List<SVGPointDrawElement> canvasElements = [];
 
-  void addSVGElement(PointDrawObject object) {
+  void addSVGElement(PointDrawObject object, Map<String, dynamic> attributes) {
+    canvasElements.add(object.toSVGElement(object.key.toString(), attributes));
+  }
+
+  String output = "";
+
+  void build(int width, int height, Rect canvasRect){
+    output = "";
+    insertXMLVersion();
+    insertSVGVersion();
+    insertSVGOpeningTag(width, height, canvasRect);
+    insertTitleTag();
+    insertDescTag();
+    buildDefinitions();
+    buildSVGElements();
+    insertSVGCloseTag();
+  }
+
+  void insertXMLVersion(){
+    output += "<?xml version=\"${xmlVersion.toStringAsFixed(1)}\"?>\n";
+  }
+
+  void insertSVGVersion(){
+    output += "<!DOCTYPE svg PUBLIC \"-//W3C//DTD "
+        "SVG ${svgVersion.toStringAsFixed(1)}//EN\" \"$docType\">\n";
+  }
+
+  void insertSVGOpeningTag(int width, int height, Rect canvasRect){
+    output += "<svg width=\"${width}\" height=\"${height}\" viewBox="
+        "\"${offsetToString(canvasRect.topLeft)} ${offsetToString(canvasRect.bottomRight)}\" "
+        "xmlns=\"http://www.w3.org/2000/svg\" "
+        "version=\"${svgVersion.toStringAsFixed(1)}\">\n";
+  }
+
+  void insertTitleTag(){
+    output += "<title>${title ?? ''}</title>\n";
+  }
+
+  void insertDescTag(){
+    output += "<desc>${desc ?? ''}</desc>\n";
+  }
+
+  void buildDefinitions(){
     // TODO:
   }
 
-//   static PointDrawCollection toPointDraw(){
-//     // TODO:
-//     return PointDrawCollection();
-//   }
+  void buildSVGElements(){
+    output += "\n\n";
+    for(SVGPointDrawElement svgElement in canvasElements){
+      output += svgElement.svgContent;
+    }
+    output += "\n\n";
+  }
+
+  void insertSVGCloseTag(){
+    output += "</svg>";
+  }
+
+  ByteData? toByteData(int width, int height, Rect canvasRect){
+    build(width, height, canvasRect);
+    List<int> stringCodes = output.codeUnits;
+    Uint8List uCodes = Uint8List.fromList(stringCodes);
+    return uCodes.buffer.asByteData();
+  }
 }
 
 class SVGPointDrawElement {
