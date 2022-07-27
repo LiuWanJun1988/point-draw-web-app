36ed48c31f1f3f22ac719b00fef7697a821e0607
LiuWanJun1988 <liuwanjun1988@gmail.com>
2022-07-27 10:32:06 +0700

    Revert "Update svg_utils.dart"
    This reverts commit 92893124de47480544460e23c4411aa520b48b15.

--- lib/point_draw_models/utilities/svg_utils.dart
+++ lib/point_draw_models/utilities/svg_utils.dart
@@ -13,23 +13,25 @@ String offsetListToString(List<Offset> offsets){
   return offsetsString;
 }
 
-String strokePaintToString(Paint? paint){
-  if(paint == null){
+String strokePaintToString(bool outlined, Paint? paint, {Map<String, dynamic>? args}){
+  if(paint == null || !outlined){
     return "stroke:none";
+  }
+  if(args?.containsKey("stroke_cap") ?? false){
+    // TODO: encode stroke caps
   }
   return "stroke:rgb(${paint.color.red},${paint.color.green},${paint.color.blue});stroke-width:${paint.strokeWidth}";
 }
 
-String fillPaintToString(Paint? paint) {
-  if(paint == null){
+String fillPaintToString(bool filled, Paint? paint, {Map<String, dynamic>? args}) {
+  if(paint == null || !filled){
     return "fill:none";
   }
-
   if (paint.shader == null) {
     return "fill:rgb(${paint.color.red},${paint.color.green},${paint.color.blue})";
+  } else {
+    return "fill:url('#${args!["shader_id"]}')\" />";
   }
-
-  return "";
 }
 
 String colorToRGB(Color? color) {
@@ -46,12 +48,7 @@ String shaderParamToString(ShaderParameters? shaderParameters, String id) {
     }
     shader += "$linearGradient$stops\n</linearGradient>";
   } else if (shaderParameters?.type == ShaderType.radial) {
-    String radialGradient = "<radialGradient id=\"$id\" cx=\"${shaderParameters?.center?.dx}\" cy=\"${shaderParameters?.center?.dy}\" r=\"${shaderParameters?.radius}\" >";
-    String stops = "";
-    for (int i = 0; i < (shaderParameters?.stops.length)!; i ++) {
-      stops += "\n<stop offset=\"${(shaderParameters?.stops[i])! * 100.0}%\" style=\"stop-color:${colorToRGB(shaderParameters?.colors[i])};stop-opacity:${shaderParameters?.colors[i].opacity}\" />";
-    }
-    shader += "$radialGradient$stops\n</radialGradient>";
+
   } else if (shaderParameters?.type == ShaderType.sweep) {
 
   }
